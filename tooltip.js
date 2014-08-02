feaa744a2c7dbd83e14de4f54951198892a89bdc
Edward <edward@edward-VirtualBox.(none)>
2014-08-01 23:39:56 -0400

    Updated to use object variables

--- tooltip.js
+++ tooltip.js
@@ -1,7 +1,9 @@
 ﻿/********************************************/
 /* Tooltip plugin - by Edward Tam, 31st July, 2014
 /********************************************/
-function Tooltip(){}
+function Tooltip(options){
+        this.parseOptions(options);
+}
 
 Tooltip.prototype.$ = jQuery;
 /************************************************************************/
@@ -33,6 +35,9 @@ Tooltip.prototype.parseOptions = function(options){
                 if (key == 'el') {
                         this.$el = this.$(options[key]);
                 }
+                if (key == 'target') {
+                        this.setTargetEl(this.$(options[key]));
+                }
         }
         return this;
 };
@@ -43,14 +48,27 @@ Tooltip.prototype.parseOptions = function(options){
 /* Description : Render content to the tooltip. Please Overwrite this if needed.
 /************************************************************************/
 Tooltip.prototype.render = function (){
-        this.show(this.$target, this.position);
+        if (this.isHover) 
+                this.show();
+        else
+                this.hide();
 };
 
+/************************************************************************/
+/* Function    : setPosition()
+/* Parameters  : position - string of either 'top', 'bottom', 'right', or 'left'
+/* Description : set the position of the tooltip relative to the target
+/************************************************************************/
 Tooltip.prototype.setPosition = function(position){
         this.position = position;
         return this;
 };
 
+/************************************************************************/
+/* Function    : setTargetEl()
+/* Parameters  : el - jQuery selector
+/* Description : Set target element
+/************************************************************************/
 Tooltip.prototype.setTargetEl = function(el){
         var self = this;
         this.$target = this.$(el);
@@ -70,18 +88,22 @@ Tooltip.prototype.setTargetEl = function(el){
 /*
 /* Description : show the tooltip, calculate position relative to hover_target
 /***************************************************************************/
-Tooltip.prototype.show = function (hover_target, position){
+Tooltip.prototype.show = function (){
         var self = this;
+
+        if (this.$el.find('.tooltip-pointer').length == 0){
+                this.$('<div/>').addClass('tooltip-pointer').appendTo(this.$el);
+        }
 
         clearTimeout(this.tooltip_timer);
         this.tooltip_timer = setTimeout(function (){
                 if (self.isHover){
-                        var offset = hover_target.offset();
+                        var offset = self.$target.offset();
 
                         /********** Default Position: top ***************/
                         var tooltip_offset = {
                                 top  : offset.top - self.$el.height() - 30, 
-                                left : offset.left + (hover_target.width() - self.$el.width())/2,
+                                left : offset.left + (self.$target.width() - self.$el.width())/2,
                         };
                         self.$el.find('.tooltip-pointer').removeClass('left right bottom').addClass('top');
 
@@ -96,13 +118,13 @@ Tooltip.prototype.show = function (hover_target, position){
                         if (self.position == 'right') {
                                 tooltip_offset = {
                                         top: offset.top,
-                                        left: offset.left + hover_target.width() + 10
+                                        left: offset.left + self.$target.width() + 10
                                 };
                                 self.$el.find('.tooltip-pointer').removeClass('top left bottom').addClass('right');
                         }
 
                         if (self.position == 'bottom') {
-                                tooltip_offset.top += hover_target.height() + self.$el.height() + 50 
+                                tooltip_offset.top += self.$target.height() + self.$el.height() + 50 
                                 self.$el.find('.tooltip-pointer').removeClass('top left right').addClass('bottom');
                         }
 
